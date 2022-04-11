71e98556e2f8a30e806d0419af482de99befce83
gparmigiani <gparmigiani@travix.com>
2022-04-11 20:32:27 +0200

    rc6 test

--- ApiStub.FSharp/Library.fs
+++ ApiStub.FSharp/Library.fs
@@ -44,12 +44,16 @@ module HttpResponseHelpers =
     let inline R_TEXT content =
         content |> R_OK "text/html"
 
-    let inline R_JSON x =
-        // this seems to be buggy, causing stream read exception with F# types!!
-        //response.Content <- JsonContent.Create(x)
+    let inline R_JSON (x : obj) =
         x
         |> System.Text.Json.JsonSerializer.Serialize
         |> R_OK "application/json"
+
+    let inline R_JSON_CONTENT (x : obj) =
+        // this seems to be buggy, causing stream read exception with F# types!!
+        let response = new HttpResponseMessage(HttpStatusCode.OK)
+        response.Content <- JsonContent.Create(inputValue=x)
+        response
 
     let inline R_ERROR statusCode content =
         let response = new HttpResponseMessage(statusCode)
@@ -166,7 +170,7 @@ module CE =
 
         /// json stub
         [<CustomOperation("stubj")>]
-        member this.StubJson(x, methods, routeTemplate, stub) =
+        member this.StubJson(x, methods, routeTemplate, stub: obj) =
             this.Stub2(x, methods, routeTemplate, stub |> R_JSON)
 
         /// stub GET request with stub function
