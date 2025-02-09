3309bbd39708177453d7416c4b6dc29614a44570
gparmigiani <gparmigiani@travix.com>
2025-02-09 13:30:02 +0100

    add fantomas ignore

---
+++ samples/web/test/Web.Sample.Test/Tests.fs
@@ -0,0 +1,30 @@
+﻿module Tests
+
+open System
+open Xunit
+open ApiStub.FSharp
+open System.Threading.Tasks
+open ApiStub.FSharp.CE
+open Web.Sample
+open System.Net.Http.Json
+
+let webAppFactory = 
+    new TestClient<Web.Sample.Program>() 
+    |> fun x -> x {
+        GETJ Web.Sample.Clients.Routes.name {| Name = "Peter" |}
+        GETJ Web.Sample.Clients.Routes.age {| Age = 100 |}
+    }
+    |> _.GetFactory()
+
+[<Fact>]
+let ``Peter is 100 years old`` () =
+    task {
+        let c = webAppFactory.CreateClient()
+
+        let! res = c.PostAsJsonAsync(Web.Sample.Services.routeOne, {| |})
+        let! responseText = res.Content.ReadAsStringAsync()
+
+        Assert.True(res.IsSuccessStatusCode)
+        Assert.Contains("Peter", responseText)
+        Assert.Contains("100", responseText)
+    }
