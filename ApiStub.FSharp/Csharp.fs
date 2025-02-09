a80a907a4669b3ad7aa61914b9e8a90f417ba22e
gparmigiani <gparmigiani@travix.com>
2025-02-09 14:29:01 +0100

    try add csharp extensions, to fix

---
+++ ApiStub.FSharp/Csharp.fs
@@ -0,0 +1,21 @@
+namespace ApiStub.Fsharp
+
+open System.Runtime.CompilerServices
+open ApiStub.FSharp.CE
+
+[<Extension>]
+type CsharpExtensions =
+
+    [<Extension>]
+    static member GETJ<'a when 'a: not struct>(x: TestClient<'a>, route: string, stub: obj) = x.GetJson(x, route, stub)
+
+    [<Extension>]
+    static member POSTJ<'a when 'a: not struct>(x: TestClient<'a>, route: string, stub: obj) =
+        x.PostJson(x, route, stub)
+
+    [<Extension>]
+    static member PUTJ<'a when 'a: not struct>(x: TestClient<'a>, route: string, stub: obj) = x.PutJson(x, route, stub)
+
+    [<Extension>]
+    static member DELETEJ<'a when 'a: not struct>(x: TestClient<'a>, route: string, stub: obj) =
+        x.DeleteJson(x, route, stub)
--- samples/web/test/Web.Sample.Csharp.Test/CSharpTests.cs
+++ samples/web/test/Web.Sample.Csharp.Test/CSharpTests.cs
@@ -4,23 +4,19 @@ using System;
 using Xunit;
 using ApiStub.FSharp;
 using System.Threading.Tasks;
-using static ApiStub.FSharp.CE;
 using Web.Sample;
 using System.Net.Http.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
+using static ApiStub.FSharp.CE;
 
 public class CSharpTests
 {
-    private static WebApplicationFactory<Web.Sample.Program> getWebAppFactory()
-    {
+    private static WebApplicationFactory<Web.Sample.Program> getWebAppFactory() =>
         // create an instance of the test client builder
-        var b = new TestClient<Web.Sample.Program>();
-
-        return 
-            b.GetJson(b, Clients.Routes.name, new { Name = "Peter" })
-            .GetJson(b, Clients.Routes.age, new { Age = 100 })
+        new TestClient<Web.Sample.Program>()
+            .GETJ(Clients.Routes.name, new { Name = "Peter" })
+            .GETJ(Clients.Routes.age, new { Age = 100 })
             .GetFactory();
-    }
 
     // one app factory instance is oke for all tests
     private static readonly WebApplicationFactory<Program> webAppFactory = getWebAppFactory();
